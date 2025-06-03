5fd330890f4430276b81073dca0b4f3f549ca032
Matt DeClaire <matt@declaire.com>
2025-06-02 17:58:16 -0700

    slack invite redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ export default defineConfig({
   site: "https://www.bmspto.com",
   integrations: [tailwind({
     applyBaseStyles: false,
-  })]
+  })],
+  redirects: {
+    "/slack": "https://join.slack.com/t/bms-parent-teacher/shared_invite/zt-36izqg9x1-pAJakGWRcBcRl_2gmn8FlA",
+  },
 });

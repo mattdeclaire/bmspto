00a7f330c87a3ef6566944470e5b4319b5418ec7
Matt DeClaire <matt@declaire.com>
2026-07-11 15:04:24 -0700

    volunteer redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,6 @@ export default defineConfig({
   },
   redirects: {
     "/slack": "https://join.slack.com/t/bms-parent-teacher/shared_invite/zt-36izqg9x1-pAJakGWRcBcRl_2gmn8FlA",
+    "/volunteer": "https://forms.gle/5DEVV71ibS2apgxA6",
   },
 });
